d858ccb30cbe2cbe3c16da3c293f35fad6f1748f
Luca Marcelli <ioncodes@users.noreply.github.com>
2020-02-16 17:59:18 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,3 @@
-# https://github.com/dracula/powershell
 # Dracula readline configuration. Requires version 2.0, if you have 1.2 convert to `Set-PSReadlineOption -TokenType`
 Set-PSReadlineOption -Color @{
     "Command" = [ConsoleColor]::Green
@@ -12,7 +11,7 @@ Set-PSReadlineOption -Color @{
 }
 # Dracula Prompt Configuration
 Import-Module posh-git
-$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]0x2192) " # arrow unicode symbol
+$GitPromptSettings.DefaultPromptPrefix.Text = "$([char]::ConvertFromUtf32(0x1F40D)) " # snake emoji
 $GitPromptSettings.DefaultPromptPrefix.ForegroundColor = [ConsoleColor]::Green
 $GitPromptSettings.DefaultPromptPath.ForegroundColor =[ConsoleColor]::Cyan
 $GitPromptSettings.DefaultPromptSuffix.Text = "$([char]0x203A) " # chevron unicode symbol
